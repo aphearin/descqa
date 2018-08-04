5e53f2de4e2d81668581e45bf4b8898c41e483b9
Andrew Hearin <andrew.hearin@yale.edu>
2018-08-03 19:33:47 -0500

    pep8 style changes caught by pylinter and flake8

--- descqa/virialscale_test.py
+++ descqa/virialscale_test.py
@@ -1,20 +1,24 @@
+"""
+"""
 from __future__ import unicode_literals, absolute_import, division
-from .plotting import plt 
+
+from .plotting import plt
 from .base import BaseValidationTest, TestResult
-import numpy as np 
-import matplotlib
+import numpy as np
 from matplotlib.colors import LogNorm
 from astropy.cosmology import WMAP7 as cosmo
 import astropy.stats as stat
 import os
-import pdb
+
 
 __all__ = ['VirialScaling']
+
+
 class VirialScaling(BaseValidationTest):
     """
-    Tests the relationship between FOF halo mass and velocity dispersion of cluster member galaxies 
+    Tests the relationship between FOF halo mass and velocity dispersion of cluster member galaxies
     """
-    def __init__(self, masscut, c_axis, stellarcut, disp_func,**kwargs):
+    def __init__(self, masscut, c_axis, stellarcut, disp_func, **kwargs):
         '''initialize the test with the folowing quantities:
         masscut: include halos with masses at least as large as specified value
         c_axis: pass 'number' for color axis to display cluster galaxy counts, and 'redshift' for cluster redshifts
@@ -25,17 +29,18 @@ class VirialScaling(BaseValidationTest):
         self.c_axis = c_axis
         self.disp_func = disp_func
         self.summary_fig, self.summary_ax = plt.subplots()
-		
+
     def run_on_single_catalog(self, catalog_instance, catalog_name, output_dir):
         '''collect quantities and plot the relationship between velocity dispersion and halo mass'''
 
-        if not catalog_instance.has_quantities(['halo_mass','halo_id', 'velocity_x', 'velocity_y', 'velocity_z', 'redshift']):
-            return TestResult(skipped = True, summary = 'do not have needed quantities')
+        if not catalog_instance.has_quantities(
+                ['halo_mass', 'halo_id', 'velocity_x', 'velocity_y', 'velocity_z', 'redshift']):
+            return TestResult(skipped=True, summary='do not have needed quantities')
 
         #list containing each galaxy's larger cluster mass
         complete_mass = catalog_instance.get_quantities('halo_mass')['halo_mass']
 
-        #sort the complete_mass list 
+        #sort the complete_mass list
         #make a list of indices corresponding to halos that make the mass cut
         mass_indices = np.argsort(complete_mass)
         complete_mass = complete_mass[mass_indices]
@@ -46,17 +51,17 @@ class VirialScaling(BaseValidationTest):
         #make a list of each galaxy's halo ID, sort it in same fashion as mass, and cut according to mass cut
         complete_id_list = catalog_instance.get_quantities('halo_id')['halo_id']
         complete_id_list = complete_id_list[mass_indices]
-        
+
         cut_id_list = complete_id_list[cut_indices]
 
         #sort the cut_id_list in increasing order and get the unique values to prepare for looping
         indexing_indices = np.argsort(cut_id_list)
         cut_id_list = cut_id_list[indexing_indices]
         unique_id_list = np.unique(cut_id_list)
-		
+
         #make a list of masses that make the mass cut and are sorted in the same fashion as the id list
         cut_masses = complete_mass[cut_indices][indexing_indices]
-        
+
         #make a list of galaxy masses, used to check galaxy mass and disregard any galaxies with low stellar masses
         stellar_masses = catalog_instance.get_quantities('stellar_mass')
         stellar_masses = stellar_masses['stellar_mass'][mass_indices][cut_indices][indexing_indices]
@@ -67,7 +72,6 @@ class VirialScaling(BaseValidationTest):
         vel_dispersion = np.array([])
         #list to contain the masses of clusters to be plotted
         mass = np.array([])
-
 
         #fetch and sort each velocity component for each galaxy according to the id list
         vx = catalog_instance.get_quantities('velocity_x')
@@ -83,7 +87,7 @@ class VirialScaling(BaseValidationTest):
 
         largest = 0
         smallest = np.max(cut_masses)
-        
+
         #list to contain redshifts of all galaxies within a particular cluster
         redshift_list = np.array([])
         #list to contain representative redshift for each cluster (determined from galaxy redshifts)
@@ -91,7 +95,7 @@ class VirialScaling(BaseValidationTest):
 
         #galaxy counter and list to store number of galaxies in each cluster
 
-        galaxy_num  = 0
+        galaxy_num = 0
         galaxy_num_list = np.array([])
 
         #function used to calculate velocity dispersion
@@ -105,7 +109,7 @@ class VirialScaling(BaseValidationTest):
         #for each cluster above the mass cut
         for unique_id in unique_id_list:
 
-            #find location of this cluster's galaxies in the list 
+            #find location of this cluster's galaxies in the list
             index = np.searchsorted(cut_id_list, unique_id)
 
             #add the cluster's mass to a list
@@ -116,39 +120,40 @@ class VirialScaling(BaseValidationTest):
             while unique_id == cut_id_list[index]:
 
                 if(stellar_masses[index] > self.stellarcut):
-                    vel_mag = np.sqrt(np.power(vx_list[index],2)+np.power(vy_list[index],2)+np.power(vz_list[index],2))
+                    vel_mag = np.sqrt(np.power(
+                        vx_list[index], 2)+np.power(vy_list[index], 2) +
+                            np.power(vz_list[index], 2))
                     vel_mag_list = np.append(vel_mag_list, vel_mag)
-                    redshift_list = np.append(redshift_list, redshifts[index])		
+                    redshift_list = np.append(redshift_list, redshifts[index])
                     galaxy_num += 1
-                
-                index+=1
+
+                index += 1
 
                 if (index == np.size(cut_id_list)):
                     break
 
             #append each calculated value for the cluster to the proper list
             galaxy_num_list = np.append(galaxy_num_list, galaxy_num)
-           
-            mask_num = galaxy_num_list>5
+
+            mask_num = galaxy_num_list > 5
             galaxy_num = 0
-	    
+
             vel_dispersion = np.append(vel_dispersion, dispersion(vel_mag_list))
-	 
+
             #use a representative, robust estimate of redshift for the whole cluster
             median_r = np.append(median_r, np.median(redshift_list))
-                        
+
             #la = cut_masses[index-1]*(cosmo.H(np.median(redshift_list)).value/100)
-            #sm = cut_masses[index-1]*(cosmo.H(np.median(redshift_list)).value/100) 
-            if(cut_masses[index-1]*((cosmo.H(np.median(redshift_list))).value/100)>largest):
+            #sm = cut_masses[index-1]*(cosmo.H(np.median(redshift_list)).value/100)
+            if(cut_masses[index-1]*((cosmo.H(np.median(redshift_list))).value/100) > largest):
                largest = cut_masses[index-1]*(cosmo.H(np.median(redshift_list)).value/100)
-            
-            if(cut_masses[index-1]*((cosmo.H(np.median(redshift_list))).value/100)<smallest):
-               smallest = cut_masses[index-1]*(cosmo.H(np.median(redshift_list)).value/100)  
-            
+
+            if(cut_masses[index-1]*((cosmo.H(np.median(redshift_list))).value/100) < smallest):
+               smallest = cut_masses[index-1]*(cosmo.H(np.median(redshift_list)).value/100)
+
             #reset lists to empty for the next iteration/cluster
             vel_mag_list = np.array([])
             redshift_list = np.array([])
-            
 
         #fig, ax = plt.subplots(nrows=1,ncols=1)
         #make different plots depending on what you want the color axis to show
@@ -156,21 +161,26 @@ class VirialScaling(BaseValidationTest):
         x_axis = np.multiply(mass[mask_num], (cosmo.H(median_r[mask_num])/100))
 
         if (self.c_axis == 'number'):
-           img = self.summary_ax.scatter(x_axis, vel_dispersion[mask_num], c = galaxy_num_list[mask_num], norm = LogNorm(), label = catalog_name + ' cluster') 
+           img = self.summary_ax.scatter(
+            x_axis, vel_dispersion[mask_num], c=galaxy_num_list[mask_num],
+            norm=LogNorm(), label=catalog_name + ' cluster')
 
         elif(self.c_axis == 'redshift'):
-           img =self.summary_ax.scatter(x_axis, vel_dispersion[mask_num], c = median_r[mask_num], norm = LogNorm(), label = catalog_name + ' cluster')	
+           img = self.summary_ax.scatter(x_axis, vel_dispersion[mask_num],
+                c=median_r[mask_num], norm=LogNorm(), label=catalog_name + ' cluster')
         #make plot
         x = np.linspace(smallest*.75, largest*1.5)
-        self.summary_ax.plot(x, eval("1082*(x/10**15)**.3361"), c = "red", label = "Evrard et al. 2007")
+        self.summary_ax.plot(x, eval("1082*(x/10**15)**.3361"),
+                c="red", label="Evrard et al. 2007")
         self.summary_ax.legend()
-        bar = self.summary_fig.colorbar(img, ax = self.summary_ax)
+        bar = self.summary_fig.colorbar(img, ax=self.summary_ax)
         self.summary_ax.set_xscale('log')
-        self.summary_ax.set_ylim(np.min(vel_dispersion[mask_num])*.3, np.max(vel_dispersion[mask_num])*5)
+        self.summary_ax.set_ylim(
+            np.min(vel_dispersion[mask_num])*.3, np.max(vel_dispersion[mask_num])*5)
         self.summary_ax.set_xlim(smallest*.75, largest*1.5)
         self.summary_ax.set_yscale('log')
-        self.summary_ax.set_xlabel('$Mh(z)  (M_{\odot})$')
-        self.summary_ax.set_ylabel('$\sigma_v$ (km/s)')
+        self.summary_ax.set_xlabel(r'$Mh(z)  (M_{\odot})$')
+        self.summary_ax.set_ylabel(r'$\sigma_v$ (km/s)')
         #label color axis depending on what you want to show
         if(self.c_axis == 'number'):
             bar.ax.set_ylabel('galaxies per cluster')
@@ -179,9 +189,9 @@ class VirialScaling(BaseValidationTest):
         plt.tight_layout()
         plt.savefig(os.path.join(output_dir, 'mass_virial_scaling.png'))
         plt.close()
-		
-        return TestResult(inspect_only = True)
-		
+
+        return TestResult(inspect_only=True)
+
     def conclude_test(self, output_dir):
         '''conclude the test'''
         self.summary_fig.savefig(os.path.join(output_dir, 'mass_virial_scaling.png'))
